83e6044a6e916eeaa5388c6695b18686298ad698
jpmare29 <jpmmare@yahoo.com>
2022-03-31 19:34:18 -0400

    finished logic, added comments

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
-
+//variable to compare current time to html elemnts data-time
 const newTime = moment().hour();
+
+//formatted date for display on page
 const displayDate = moment().format('MMM Do YYYY');
 $('#currentDay').text(displayDate);
 
+//getting div elements for each row of time
 const $9AmDiv = $('#row1');
 const $10AmDiv = $('#row2');
 const $11AmDiv = $('#row3');
@@ -13,16 +16,12 @@ const $3PmDiv = $('#row7');
 const $4PmDiv = $('#row8');
 const $5PmDiv = $('#row9');
 
-
+//array for testing div elements against current time later
 const testArray = [$9AmDiv, $10AmDiv, $11AmDiv, $12PmDiv, $1PmDiv, $2PmDiv, $3PmDiv, $4PmDiv, $5PmDiv];
 
-console.log($9AmDiv.attr('id'));
-
-localStorage.setItem($9AmDiv.attr('id'), $9AmDiv.children('.col-10').text());
-console.log(localStorage.getItem($9AmDiv.attr('id')));
-
+//array function to compare current time to div element time
+//and set background color accordingly
 testArray.forEach(element => {
-
     let query = element.data('time');
     if (query < newTime) {
         element.children('.col-10').addClass('past');
@@ -33,17 +32,36 @@ testArray.forEach(element => {
     }
 });
 
+//testing to see if each div has an associated event in localStorage
+//if they don't key is initialized to ''
+//if they do then the div text is set to the key value
+testArray.forEach(element => {
+    let testEvent = localStorage.getItem(element.attr('id'));
+    if(!testEvent) {
+        localStorage.setItem(element.attr('id'), '')
+    } else {
+        element.children('.col-10').text(testEvent);
+    }
+})
+
+//logic for changing div to textarea on click and preserving any text inside
+//once clicked user can update event inside of the div
+//it also activates the save button sibling to the div so that
+//the save logic can be employed properly
 $('.col-10').on('click', event => {
-    const $modifier = $(event.currentTarget)
+    let $modifier = $(event.currentTarget)
     $modifier.siblings('.saveBtn').addClass('active');
-    console.log($modifier.siblings('.saveBtn'));
     let initialText = $modifier.text();
     let $textArea = $("<textarea class='col-10'></textarea>" );
     $textArea.text(initialText);
     $modifier.replaceWith($textArea);
     $textArea.focus();
+    //save to localStorage logic and add updated text back to div
+    //also added the bckground color back before deciding to refresh the page at the end
     $('.active').on('click', event => {
         let savedText = $(event.currentTarget).siblings('.col-10').val();
+        let saveKey = $(event.currentTarget).parent().attr('id');
+        localStorage.setItem(saveKey, savedText);
         let $newDiv = $("<div class='col-10'></div>")
         $newDiv.text(savedText);
         if ($modifier.hasClass('past')) {
@@ -54,7 +72,9 @@ $('.col-10').on('click', event => {
             $newDiv.addClass('future');
         }
         $textArea.replaceWith($newDiv);
+        //added a refresh because div could not be re-edited a second time
+        //this also removes the active class from the save button
+        document.location.reload();
     })
 
 });
-
